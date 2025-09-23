22a215731bbd1f3281ce269530c3ae54308c8cfa
QDoWeber <quando@mail.weber.edu>
2025-09-22 23:33:04 -0600

    Update FitnessTrackingApp.java

--- Portfolio/FitnessTrackingApp.java
+++ Portfolio/FitnessTrackingApp.java
@@ -1,5 +1,21 @@
 package Portfolio;
+import java.util.ArrayList;
+
 
 public class FitnessTrackingApp {
-    
+    private ArrayList<User> users;
+
+
+    public FitnessTrackingApp() {
+        this.users = new ArrayList<>();
+    }
+
+
+    public ArrayList<User> getUsers() { return users; }
+
+
+    // Methods
+    public void registerUser(User user) {}
+    public void showUserProgress(User user) {}
 }
+
